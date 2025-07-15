5542a918f398aaae7daed91c0c8d9725ad1ee949
Mark Li <mkli627554@gmail.com>
2025-07-14 21:17:28 -0500

    set up client

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+const API_BASE = process.env.API_URL;
+
+export default function LoginPage() {
+
+    return (
+      <main className="flex flex-col items-center justify-center h-screen">
+      <a href={API_BASE + '/login'}>
+      <div className="bg-green-400 p-4 rounded-md">
+        <h1 className="text-2xl font-bold py-2 text-center">Login with Spotify</h1>
+      </div>
+      </a>
+    </main>
+    );
+  }
--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, jsonify, request
 from main import get_current_playing
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -7,11 +7,12 @@ import os
 
 load_dotenv()
 
-REDIRECT_URL = os.getenv('REDIRECT_URL')
+REDIRECT_URL = os.getenv('REDIRECT_URI')
 
 app = Flask(__name__)
 
 scope = "user-top-read user-read-playback-state playlist-read-private playlist-read-collaborative"
+auth_manager = SpotifyOAuth(scope=scope, redirect_uri=REDIRECT_URL)
 
 @app.route('/')
 def index():
@@ -19,15 +20,39 @@ def index():
 
 @app.route('/current_playing')
 def current_playing():
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, redirect_uri=REDIRECT_URL))
-    user = sp.current_user()
-
+    
+    sp = spotipy.Spotify(auth_manager=auth_manager)
     playback = get_current_playing(sp, verbose=True)
     print(playback)
     return jsonify({
-        'user': user,
         'message': playback['item'] if playback else None
         })
 
+@app.route('/login')
+def login():
+    auth_url = auth_manager.get_authorize_url()
+    print(auth_url, REDIRECT_URL)
+    # sp = spotipy.Spotify(auth_manager=auth_manager)
+    # user = sp.current_user()
+
+    # return jsonify({
+    #     'user': user,
+    #     'message': 'Success'
+    #     })
+
+    return redirect(auth_url)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    token_info = auth_manager.get_access_token(code)
+    access_token = token_info['access_token']
+    sp = spotipy.Spotify(auth=access_token)
+    user = sp.current_user()
+    return jsonify({
+        'user': user,
+        'message': 'Logged in successfully!'
+    })
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
