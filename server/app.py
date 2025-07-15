a0845269a7a8fc93134bb23ae575b3f7b0510f28
Mark Li <mkli627554@gmail.com>
2025-07-15 02:42:08 -0500

    created login page

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, jsonify, request
+from flask import Flask, redirect, jsonify, request, session
 from main import get_current_playing
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 scope = "user-top-read user-read-playback-state playlist-read-private playlist-read-collaborative"
 auth_manager = SpotifyOAuth(scope=scope, redirect_uri=REDIRECT_URL)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 @app.route('/')
 def index():
@@ -20,8 +21,6 @@ def index():
 
 @app.route('/current_playing')
 def current_playing():
-    
-    sp = spotipy.Spotify(auth_manager=auth_manager)
     playback = get_current_playing(sp, verbose=True)
     print(playback)
     return jsonify({
@@ -31,28 +30,21 @@ def current_playing():
 @app.route('/login')
 def login():
     auth_url = auth_manager.get_authorize_url()
-    print(auth_url, REDIRECT_URL)
-    # sp = spotipy.Spotify(auth_manager=auth_manager)
-    # user = sp.current_user()
-
-    # return jsonify({
-    #     'user': user,
-    #     'message': 'Success'
-    #     })
 
     return redirect(auth_url)
+
+app.secret_key = 'secret_key'
 
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
     token_info = auth_manager.get_access_token(code)
     access_token = token_info['access_token']
-    sp = spotipy.Spotify(auth=access_token)
+    session['access_token'] = access_token
+
     user = sp.current_user()
-    return jsonify({
-        'user': user,
-        'message': 'Logged in successfully!'
-    })
+
+    return redirect(f"http://localhost:3000/dashboard?user={user['display_name']}")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
