e355c4ca6051f4cc6c6748bf23c3dc9f509613ab
Mark Li <mkli627554@gmail.com>
2025-07-26 00:54:03 -0500

    dashboard formatting, skip songs confirm screen

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -21,35 +21,40 @@ export default function Dashboard() {
   const currentPlaying = getNowPlaying();
 
   return (
-    <main className='p-12 grid grid-cols-1 md:grid-cols-3 gap-8'>
-      <div className='flex flex-col items-center'>
-      <div className='flex flex-col bg-green-400 rounded-lg p-6 transition-all duration-300 max-w-84 md:max-w-96 min-h-128'>
-        <h1 className="text-3xl font-bold flex justify-center items-center mb-2">Currently Playing</h1>
-        <hr className='w-full mb-2'/>
-        <div className="flex w-full justify-between items-center">
-          <p className='text-sm mb-2'><span className="underline">Username</span>: {user}</p>
-          <p className='font-semibold mb-2'>{currentPlaying.status}</p>
+    <main className='w-full flex items-center justify-center py-12'>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-20 h-120 md:h-132'>
+      <div className='flex flex-col items-center justify-center w-84 md:w-96 h-full'>
+        <div className='flex flex-col bg-green-400 rounded-lg p-6 w-84 md:w-96 h-full'>
+          <h1 className="text-3xl font-bold flex justify-center items-center mb-2">Currently Playing</h1>
+          <hr className='w-full mb-2'/>
+          <div className="flex w-full justify-between items-center">
+            <p className='text-sm mb-2'><span className="underline">Username</span>: {user}</p>
+            <p className='font-semibold mb-2'>{currentPlaying.status}</p>
+          </div>
+          {currentPlaying.name !== "" && (
+            <div className="w-full flex flex-col items-center">
+              <img src={currentPlaying.image} alt="Album Art" className='w-full h-full mb-2'/>
+              <p className='text-xl font-bold text-left w-full'>{currentPlaying.name}</p>
+              <p className='text-sm text-left w-full'>By: <span className='font-bold'>{currentPlaying.artists}</span></p>
+            </div>
+          )}
+          {currentPlaying.name === "" && (
+            <div className="w-full flex justify-center items-center">
+              <p className='text-2xl font-bold border-2 border-white rounded-sm p-4 my-5'>Nothing is playing</p>
+            </div>
+          )}
         </div>
-        {currentPlaying.name !== "" && (
-          <div className="w-full flex flex-col items-center">
-            <img src={currentPlaying.image} alt="Album Art" className='w-full h-full mb-2'/>
-            <p className='text-xl font-bold text-left w-full'>{currentPlaying.name}</p>
-            <p className='text-sm text-left w-full'>By: <span className='font-bold'>{currentPlaying.artists}</span></p>
-          </div>
-        )}
-        {currentPlaying.name === "" && (
-          <div className="w-full flex justify-center items-center">
-            <p className='text-2xl font-bold border-2 border-white rounded-sm p-4 my-5'>Nothing is playing</p>
-          </div>
-        )}
       </div>
-      </div>
-      <div className='flex flex-col items-center bg-red-200'>
+      
+      <div className='flex flex-col items-center bg-red-200 rounded-lg p-6 w-84 md:w-96'>
         Queue
       </div>
-      <div className='flex flex-col items-center bg-blue-200'>
+
+      <div className='flex flex-col items-center bg-blue-200 rounded-lg p-6 w-84 md:w-96'>
         Recommended
       </div>
+      </div>
+      
     </main>
   );
 }
--- client/src/app/skipped/page.tsx
+++ client/src/app/skipped/page.tsx
@@ -8,23 +8,37 @@ const API_BASE = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Skipped() {
   const [skippedSongs, setSkippedSongs] = useState<{ [id: string]: { name: string, artist: string, uri: string, strikes: number, image: string } }>({});
+  const [showConfirmScreen, setShowConfirmScreen] = useState(false);
+  const [action, setAction] = useState<(() => void) | null>(null);
 
   const currentPlaying = getNowPlaying();
 
   function removeFromPlaylist(): void {
+    // TODO: Implement this
     console.log("Removing from playlist");
   }
 
-  function clearSkippedSongs(): void {
-    console.log("Clearing skipped songs");
+  function clearSongs(): void {
     fetch(API_BASE + '/clear_skipped_songs')
       .then(res => res.json())
       .then(data => {
         console.log(data.message)
       });
   }
-  
 
+  function clearSkippedSongs(): void {
+    setAction(() => clearSongs);
+    setShowConfirmScreen(true);
+  }
+
+  function handleConfirm(button: 1 | 0) : void {
+    if (button == 1 && action) {
+      action();
+    }
+    setAction(null);
+    setShowConfirmScreen(false);
+  }
+  
   useEffect(() => {
     fetch(API_BASE + '/refresh_skipped')
       .then(res => res.json())
@@ -65,9 +79,20 @@ export default function Skipped() {
           <div className="flex flex-col items-center justify-center py-4 px-3 bg-green-500 rounded-lg w-full cursor-pointer hover:bg-green-600" onClick={removeFromPlaylist}>
             <h1 className="text-xs md:text-xl font-bold text-white">Remove from playlist</h1>
           </div>
-          <div className="flex flex-col items-center justify-center py-4 px-3 bg-red-500 rounded-lg w-full cursor-pointer" onClick={clearSkippedSongs}>
+          <div className="flex flex-col items-center justify-center py-4 px-3 bg-red-500 rounded-lg w-full cursor-pointer hover:bg-red-600" onClick={clearSkippedSongs}>
             <h1 className="text-xs md:text-xl font-bold text-white">Clear skipped songs</h1>
           </div>
+        </div>
+      )}
+      {showConfirmScreen && (
+        <div className="bg-white fixed w-screen h-screen flex items-center justify-center top-0 left-0">
+          <div className="flex flex-col items-center justify-center gap-4 p-6">
+          <h1 className="text-2xl font-bold">Are you sure?</h1>
+          <div className="flex items-center justify-center gap-4 w-full md:w-128">
+            <button className="flex flex-col items-center justify-center p-4 w-30 md:w-50 md:h-16 bg-green-500 rounded-lg cursor-pointer hover:bg-green-600" onClick={() => handleConfirm(1)}>Yes</button>
+            <button className="flex flex-col items-center justify-center p-4 w-30 md:w-50 md:h-16 bg-red-500 rounded-lg cursor-pointer hover:bg-red-600" onClick={() => handleConfirm(0)}>No</button>
+          </div>
+        </div>
         </div>
       )}
     </div>
